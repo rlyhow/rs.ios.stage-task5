e1d0897ae8a6525155883793cc74d01e5b668b65
Mikita Abramowicz <gentled.druzhinnik@gmail.com>
2021-06-13 16:31:32 +0300

    feat: implement exercise-1 and exercise-2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,26 @@
 import Foundation
 
 class StockMaximize {
-
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var sumOfBought = 0
+        var countOfStocks = 0
+        var finalProfit = 0
+        
+        for i in prices.indices {
+            if i == prices.count-1 {
+                finalProfit += prices[i] * countOfStocks - sumOfBought
+                break
+            }
+            if prices[i] <= prices[i+1] {
+                sumOfBought += prices[i]
+                countOfStocks+=1
+            } else {
+                finalProfit += prices[i] * countOfStocks - sumOfBought
+                countOfStocks = 0
+                sumOfBought = 0
+            }
+        }
+        return finalProfit
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -2,7 +2,74 @@ import UIKit
 
 class MessageDecryptor: NSObject {
     
+    let regex = "([0-9]+)?\\[[a-z]+\\]"
+    let regex2 = "[0-9]+"
+    let regex3 = "[a-z]+"
+    let regex4 = "^[a-z]+$"
+    
     func decryptMessage(_ message: String) -> String {
-        return ""
+        if message.count < 1 || message.count > 60 { return "" }
+        let correctSymbols: Set<Character> = ["0","1","2","3","4","5","6","7","8","9",
+                                    "a","b","c","d","e","f","g","h","i","j","k",
+                                    "l","m","n","o","p","q","r","s","t","u","v",
+                                    "w","x","y","z","[","]"]
+        var countOfBrackets = 0
+        for i in message {
+            if !correctSymbols.contains(i) { return "" }
+            if i == "[" {
+                countOfBrackets+=1
+            } else if i == "]" {
+                countOfBrackets-=1
+            }
+            if countOfBrackets < 0 { return "" }
+        }
+        if countOfBrackets != 0 { return "" }
+        
+        var mes = message
+        var result = matchString(for: regex, in: mes)
+        while true {
+            if result.isEmpty {
+                mes = matchString(for: regex4, in: mes)
+                break
+            } else {
+                let countOfRepeat = Int(matchNumber(for: regex2, in: result))!
+                if countOfRepeat < 1 || countOfRepeat > 300 { return "" }
+                let stringInBrackets = matchString(for: regex3, in: result)
+                var newRepeatedString = ""
+                for _ in 0 ..< countOfRepeat {
+                    newRepeatedString+=stringInBrackets
+                }
+                mes = mes.replacingOccurrences(of: result, with: newRepeatedString)
+            }
+            result = matchString(for: regex, in: mes)
+        }
+        return mes
     }
+    
+    func matchString(for regex: String, in text: String) -> String {
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let result = regex.firstMatch(in: text, range: NSRange(text.startIndex..., in: text))
+            
+            return result.map {String(text[Range($0.range, in: text)!])} ?? ""
+            
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return ""
+        }
+    }
+    
+    func matchNumber(for regex: String, in text: String) -> String {
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let result = regex.firstMatch(in: text, range: NSRange(text.startIndex..., in: text))
+            
+            return result.map {String(text[Range($0.range, in: text)!])} ?? "1"
+            
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return "0"
+        }
+    }
+    
 }
