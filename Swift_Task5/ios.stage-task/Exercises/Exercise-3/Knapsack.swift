4ea2c4f9d6d2260a2bcdc679995b79610d7d3114
Mikita Abramowicz <gentled.druzhinnik@gmail.com>
2021-06-14 21:41:10 +0300

    feat: implement exercise-3:Knapsack

--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -17,6 +17,36 @@ public final class Knapsack {
     }
     
     func findMaxKilometres() -> Int {
-        return -1
+        
+        let countOfFood = foods.count
+        let countOfDrink = drinks.count
+        
+        let food = knapSack(W: maxWeight, wt: foods, val: foods, n: countOfFood)
+        let drink = knapSack(W: maxWeight, wt: drinks, val: drinks, n: countOfDrink)
+        
+        var maxKilometres = [Int]()
+        for i in 0...maxWeight {
+            maxKilometres.append(min(food[countOfFood][i], drink[countOfDrink][maxWeight-i]))
+        }
+        
+        return maxKilometres.max() ?? 0
+    }
+    
+    func knapSack(W :Int, wt: [Supply], val: [Supply], n: Int) -> [[Int]] {
+        
+        var K = Array(repeating: Array(repeating: 0, count: W + 1), count: n + 1)
+        
+        for i in 0...n {
+            for w in 0...W  {
+                if i == 0 || w == 0 {
+                    K[i][w] = 0
+                } else if wt[i-1].weight <= w {
+                    K[i][w] = max(val[i-1].value + K[i-1][w - wt[i-1].weight],  K[i-1][w]);
+                } else {
+                    K[i][w] = K[i-1][w]
+                }
+            }
+        }
+        return K;
     }
 }
